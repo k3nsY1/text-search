359ca6dcd3c90e1b2036737093449ef5997b09a1
unknown <tima02021996@gmail.com>
2022-06-23 19:58:22 +0600

    added comments

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"os"
 )
 
-type Word struct {
-	Bytes []byte
-	// word []byte
-	Count int
-}
+// type Word struct {
+// 	Bytes []byte
+// 	// word []byte
+// 	Count int
+// }
 
 func main() {
 	file, err := os.Open("mobydick.txt")
@@ -27,39 +27,55 @@ func main() {
 
 	for scanner.Scan() {
 		// words := scanner.Text()
-		// r, _ := utf8.DecodeRune(words[])
-		words = append(words, scanner.Bytes())
 
+		// Запись слов в нижний регистр и избавление от знаков
+		for i := 0; i < len(scanner.Bytes()); i++ {
+			if scanner.Bytes()[i] >= 65 && scanner.Bytes()[i] <= 90 {
+				scanner.Bytes()[i] = scanner.Bytes()[i] + 32
+				words = append(words, scanner.Bytes())
+			} else if scanner.Bytes()[i] == 32 || scanner.Bytes()[i] == 42 {
+				scanner.Bytes()[i] = 0
+				words = append(words, scanner.Bytes())
+			} else {
+				words = append(words, scanner.Bytes())
+			}
+		}
 		// fmt.Println(scanner.Text())
 	}
 	// for i := 0; i < len(words); i++ {
 	// 	fmt.Println(words[i])
 	// }
+
 	var first [][]byte
 	var second []int
+	// Пробегаемся по циклу и если words не нулевой, то сравниваем два элемента, первый со следующим
 	for i := 0; i < len(words); i++ {
 		if words[i] != nil {
 			var cnt = 1
 			for j := i + 1; j < len(words); j++ {
 				res := bytes.Compare(words[i], words[j])
+				// Если равны, то добавляем в счетчик и удаляем элемент
 				if res == 0 {
 					cnt++
 					words[j] = nil
 				}
 			}
+			// В первый добавляем слово
 			first = append(first, words[i])
+			// Во второй количество
 			second = append(second, cnt)
 		}
 	}
+	// Сортируем массивы по количеству
 	for i := 0; i < len(first); i++ {
 		for j := i + 1; j < len(first); j++ {
 			if second[i] < second[j] {
 				second[i], second[j] = second[j], second[i]
 				first[i], first[j] = first[j], first[i]
-				// swa(second[i], second[j])
 			}
 		}
 	}
+	// Выводим 20 элементов
 	for i := 0; i < len(first); i++ {
 		fmt.Println(string(first[i]), second[i])
 		if i == 19 {
