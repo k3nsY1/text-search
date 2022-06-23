78fc5a5e7ac8b9e74ccbd574fc7dc7eb130f96c1
unknown <tima02021996@gmail.com>
2022-06-23 17:21:07 +0600

    added algo

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 )
@@ -29,8 +30,43 @@ func main() {
 		// r, _ := utf8.DecodeRune(words[])
 		words = append(words, scanner.Bytes())
 
-		fmt.Println(scanner.Text())
+		// fmt.Println(scanner.Text())
 	}
+	// for i := 0; i < len(words); i++ {
+	// 	fmt.Println(words[i])
+	// }
+	var first [][]byte
+	var second []int
+	for i := 0; i < len(words); i++ {
+		if words[i] != nil {
+			var cnt = 1
+			for j := i + 1; j < len(words); j++ {
+				res := bytes.Compare(words[i], words[j])
+				if res == 0 {
+					cnt++
+					words[j] = nil
+				}
+			}
+			first = append(first, words[i])
+			second = append(second, cnt)
+		}
+	}
+	for i := 0; i < len(first); i++ {
+		for j := i + 1; j < len(first); j++ {
+			if second[i] < second[j] {
+				second[i], second[j] = second[j], second[i]
+				first[i], first[j] = first[j], first[i]
+				// swa(second[i], second[j])
+			}
+		}
+	}
+	for i := 0; i < len(first); i++ {
+		fmt.Println(string(first[i]), second[i])
+		if i == 19 {
+			break
+		}
+	}
+
 	// resArray := []Word{}
 	// for i := 0; i < len(words); i++ {
 	// 	currentWord := Word{
